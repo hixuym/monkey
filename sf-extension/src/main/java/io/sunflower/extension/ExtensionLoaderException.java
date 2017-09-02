c46a36558cb2542aa2435a6a772aa21a386c48e6
Michael <hixuym@126.com>
2017-09-02 16:35:51 +0800

    增加sf-extension模块，替代java.util.ServiceLoader

---
+++ sf-extension/src/main/java/io/sunflower/extension/ExtensionLoaderException.java
@@ -0,0 +1,26 @@
+package io.sunflower.extension;
+
+/**
+ * Created by michael on 17/9/2.
+ */
+public class ExtensionLoaderException extends RuntimeException {
+    public ExtensionLoaderException() {
+        super();
+    }
+
+    public ExtensionLoaderException(String message) {
+        super(message);
+    }
+
+    public ExtensionLoaderException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ExtensionLoaderException(Throwable cause) {
+        super(cause);
+    }
+
+    protected ExtensionLoaderException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
