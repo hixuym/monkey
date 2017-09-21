43a6c5a9135e987af0d745755084609429a2378d
Michael <hixuym@126.com>
2017-09-21 18:19:03 +0800

    fixed sf-core tests.

--- sf-core/src/test/java/io/sunflower/cli/CommandTest.java
+++ sf-core/src/test/java/io/sunflower/cli/CommandTest.java
@@ -71,7 +71,7 @@ public class CommandTest {
                     "positional arguments:%n" +
                     "  {a,b,c}                Type to use%n" +
                     "%n" +
-                    "optional arguments:%n" +
+                    "named arguments:%n" +
                     "  -h, --help             show this help message and exit%n"
             ));
 
